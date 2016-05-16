90aedc5e8a1fc00027689560fa2fe793882c9859
mimos1010 <zwitztheone@gmail.com>
2016-05-16 12:43:50 -0400

    Create main.h
    precompiled headers and stuff

---
+++ main.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Main.hpp>
+#include <SFML\Config.hpp>
+
+#include <cstdlib>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include <random>
+#include <string>
+
